5b35d2b4c4adc67fb49c04bbcd5b1f686ea97880
Udit <uditde.com@gmail.com>
2023-05-12 00:27:07 +0530

    nav bar added

--- main.py
+++ main.py
@@ -11,6 +11,38 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.browser)
         self.showMaximized()
 
+        # nav
+        navbar = QToolBar()
+        self.addToolBar(navbar)
+
+        back_btn = QAction("Back", self)
+        back_btn.triggered.connect(self.browser.back)
+        navbar.addAction(back_btn)
+
+        forward_btn = QAction("Forward", self)
+        forward_btn.triggered.connect(self.browser.forward)
+        navbar.addAction(forward_btn)
+
+        reload_btn = QAction("Reload", self)
+        reload_btn.triggered.connect(self.browser.reload)
+        navbar.addAction(reload_btn)
+
+        home_btn = QAction("Home", self)
+        home_btn.triggered.connect(self.navigate_home)
+        navbar.addAction(home_btn)
+
+        self.url_bar = QLineEdit()
+        self.url_bar.returnPressed.connect(self.navigate_to_url)
+        navbar.addWidget(self.url_bar)
+
+
+    def navigate_home(self):
+        self.browser.setUrl(QUrl("http://google.com"))
+    
+    def navigate_to_url(self):
+        url = self.url_bar.text()
+        self.browser.setUrl(QUrl(url))
+
 
 app = QApplication(sys.argv)
 QApplication.setApplicationName("Wiser")
